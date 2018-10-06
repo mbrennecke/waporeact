1619d7ddadeabdd2c890ec782a182aba1066f24a
Matthew Brennecke <mbrennecke10@gmail.com>
2018-10-05 19:30:12 -0600

    books is now articles

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,10 +1,9 @@
 import React from "react";
 
-const Card = ({ children }) => (
+export const Card = ({ children }) => (
   <div className="card"
-	style={{ padding: 10, textAlign: "center", marginBottom: 15 }}>
+	style={{ padding: 10, marginBottom: 15 }}>
     {children}
   </div>
 );
 
-export default Card;
--- models/article.js
+++ models/article.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
   topic: { type: String, required: true },
-  year: { type: String, required: true },
+  pubdate: { type: Date, default: Date.now },
   summary: String,
   date: { type: Date, default: Date.now }
 });
